5ff060780de7293a30ea1ad6b417de80e7d5db9f
certifiedkennedy <certifiedkennedy@users.noreply.github.com>
2018-04-25 14:05:00 -0400

    Functioning app in Android and UWP. UWP still fails frequently; presumably due to timeout becauuse of image file size. Incomplete multithreaded implementation.

--- fruit_inspector/fruit_inspector/MainPage.xaml.cs
+++ fruit_inspector/fruit_inspector/MainPage.xaml.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Microsoft.Cognitive.CustomVision.Prediction;
+using Microsoft.Cognitive.CustomVision.Prediction.Models;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
 
 namespace fruit_inspector
 {
@@ -14,17 +18,86 @@ namespace fruit_inspector
 			InitializeComponent();
 
             CameraButton.Clicked += CameraButton_Clicked;
-            
-		}
+
+            SuccessButton.IsVisible = false;
+            FailureButton.IsVisible = false;
+        }
 
         private async void CameraButton_Clicked(object sender, EventArgs e)
         {
-            Plugin.Media.Abstractions.MediaFile photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+//#if WINDOWS_PHONE
+            //todo
+            await CrossMedia.Current.Initialize();
+            PhotoImage.Source = "";
+            ResultLabel.Text = "";
+//#endif
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert(
+                    "No Camera",
+                    ":( No camera available.",
+                    "OK");
+                return;
+            }
+
+            //MediaFile photo = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions()
+            //    {
+            //        Directory = "FruitInspector",
+            //        Name = "test.jpg"
+            //    });
+            MediaFile photo = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions(){ });
 
             if (photo != null)
             {
-                PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+                PhotoImage.Source = ImageSource.FromFile(photo.Path);
+                //PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+
+                string prediction = await RecognizePicture(photo);
+                ResultLabel.Text = prediction; //RecognizePicture(photo);
+                ResultLabel.IsVisible = true;
             }
+
+
+            // 2. Overlay image with text representing results of prediction
+            // 3. Enable success/failure buttons to affirm/unaffirm results
+            // 4. Send image/tag to API for improvement of model
+            // 5. Retrain model
         }
-	}
+        private async Task<string> RecognizePicture(MediaFile file)
+        {
+            string message = "Nothing recognized...";
+
+            try
+            {
+                // 1. Send to CustomVision.ai
+                string predictionKey = "ec639b00591d498bba6da92b47f88262";
+                PredictionEndpoint endpoint = new PredictionEndpoint() { ApiKey = predictionKey };
+                string projectId = "55dfeb9d-02f4-4a57-9b8d-2d132f0a2b9c";
+                var result = endpoint.PredictImage(Guid.Parse(projectId), file.GetStream());
+                
+                double prob = 0;
+                foreach (var c in result.Predictions)
+                {
+                    if (c.Probability > 0.7)
+                    {
+                        message = c.Tag;
+                        prob = c.Probability;
+                    }
+                    else if (c.Probability > prob)
+                    {
+                        prob = c.Probability;
+                        message = "Maybe " + c.Tag;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+            }
+
+            return message;
+        }
+
+    }
 }
